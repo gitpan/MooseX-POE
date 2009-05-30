14e3df03b5b1383307ac4755cfc7fa85db1ff4c0
Chris Prather <chris@prather.org>
2009-05-30 04:20:08 +0000

    Import of PERIGRIN/MooseX-POE-0.203 from CPAN.
    gitpan-cpan-distribution: MooseX-POE
    gitpan-cpan-version:      0.203
    gitpan-cpan-path:         PERIGRIN/MooseX-POE-0.203.tar.gz
    gitpan-cpan-author:       PERIGRIN
    gitpan-cpan-maturity:     released

--- lib/MooseX/POE/Meta/Trait/Constructor.pm
+++ lib/MooseX/POE/Meta/Trait/Constructor.pm
@@ -2,6 +2,10 @@ package MooseX::POE::Meta::Trait::Constructor;
 
 use Moose::Role;
 
+# For CMOP 0.82_01+
+sub _expected_method_class { "MooseX::POE::Meta::Trait::Object" }
+
+# For older versions of Moose/CMOP
 sub _expected_constructor_class { "MooseX::POE::Meta::Trait::Object" }
 
 around _generate_instance => sub {
@@ -33,3 +37,41 @@ EOF
 no Moose::Role;
 
 1;
+
+__END__
+
+=head1 NAME
+
+MooseX::POE::Meta::Trait::Constructor
+
+=head1 VERSION
+
+This documentation refers to version 0.01.
+
+=head1 SYNOPSIS
+
+use MooseX::POE::Meta::Trait::Constructor;
+
+=head1 DESCRIPTION
+
+The MooseX::POE::Meta::Trait::Constructor class implements ...
+
+=head1 SUBROUTINES / METHODS
+
+There are no public methods.
+
+=head1 DEPENDENCIES
+
+Moose::Role
+
+=head1 AUTHOR
+
+Chris Prather (chris@prather.org)
+
+=head1 LICENCE
+
+Copyright 2009 by Chris Prather.
+
+This software is free.  It is licensed under the same terms as Perl itself.
+
+=cut
